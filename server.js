d1eba05e73886a2dfbe5845ebf7873fd23889a62
Vianey Marcelo <vianymoz@gmail.com>
2022-02-08 19:02:33 -0600

    Add new port

--- server.js
+++ server.js
@@ -24,13 +24,13 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => { res.send(db.select('*').from('users'))});
+app.get('/', (req, res) => { res.send("it is working"))});
 app.post('/signin', (req, res) => signin.handleSignIn( req, res, db, bcrypt ));
 app.post('/register', (req, res) => register.handleRegister( req, res, db, bcrypt ));
 app.get('/profile/:id', (req, res) => profile.handleProfile(req, res));
 app.post('/imageUrl', (req, res) => image.handleApiCall(req, res));
     
-app.listen(3000, ()=>{
-    console.log('app is running on port 3000');
+app.listen(process.env.PORT || 3000, ()=>{
+    console.log(`app is running on port ${process.env.PORT}`);
 })
 
